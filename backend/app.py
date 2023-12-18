d6ce362cf9f6d6e7e8079113b3c1b7ba1674319a
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-12-18 13:43:14 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,11 @@ def handle_post_request():
     data = request.get_json()
     # Faça algo com os dados do POST
     return {'message': 'POST recebido com sucesso'}
+@app.route('/api/', methods=['GET'])
+def handle_post_request():
+    data = request.get_json()
+    # Faça algo com os dados do POST
+    return {'message': 'GET recebido com sucesso'}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
