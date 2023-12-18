55a653f08f875ac26eced82356d3f15e9edcde22
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-12-18 13:51:00 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -16,4 +16,5 @@ def handle_get_request():
     return {'message': 'GET recebido com sucesso'}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
